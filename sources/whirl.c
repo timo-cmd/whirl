6ed38b55c6d0b55b6064fba215d3f96c692f1123
Timo S <58859419+timo-cmd@users.noreply.github.com>
2021-01-04 17:36:25 +0100

    Update whirl.c

--- sources/whirl.c
+++ sources/whirl.c
@@ -1,11 +1,19 @@
 #include <stdlib.h>
 #include <string.h>
-#include <vector.h>
 #include <stdio.h>
+
+FILE* file;
+
+unsigned static int program[100000];
 
 typedef enum int mem_t;
 typedef enum bool prog_t;
 
 bool opsring() {
+  typedef struct {
+    POS1,
+  } opsring;
+
   return opsring;
 }
+
